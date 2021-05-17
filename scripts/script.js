a1088e221c5762895d0bfb5a495201e0da8311fb
roseybugayon <rosey.bugayong13@gmail.com>
2021-05-16 22:22:04 -0700

    changed more with fetch

--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,6 @@
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
-// Make sure you register your service worker here too
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
@@ -46,6 +33,18 @@ document.querySelector('h1').addEventListener('click', () => {
   setState({page: "home page"}, false);
 });
 
+// Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 
 
--- sw.js
+++ sw.js
@@ -8,23 +8,17 @@ var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     'https://cse110lab6.herokuapp.com/entries'
 ];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
-
-self.addEventListener('activate', event => {
-    event.waitUntil(clients.claim());
-  });
-
-  self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
             .then(function (response) {
@@ -50,4 +44,8 @@ self.addEventListener('activate', event => {
                 );
             })
     );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
 });
