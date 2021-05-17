a68cb5ac7c0ddb4d7cb6170f399e99ebe4d7b42b
roseybugayon <rosey.bugayong13@gmail.com>
2021-05-16 21:35:24 -0700

    hopefully this works

--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@
 //   - One for fetch requests
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-    'https://cse110lab6.herokuapp.com/entries'
+    'https://cse110lab6.herokuapp.com/entries',
+    './'
 ];
 
 self.addEventListener('install', function(event) {
