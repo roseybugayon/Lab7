5324849a7db799ce242be9906e6c7a03d0af312d
roseybugayon <rosey.bugayong13@gmail.com>
2021-05-16 22:15:24 -0700

    added longer fetch

--- sw.js
+++ sw.js
@@ -24,16 +24,30 @@ self.addEventListener('activate', event => {
     event.waitUntil(clients.claim());
   });
 
-  self.addEventListener('fetch', function(event) {
+  self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
     );
 });
